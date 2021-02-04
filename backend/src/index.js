41fe071d321980f08780335e70460fa1b25b74ad
KaesarZ <kaesar.dev@gmail.com>
2021-02-04 10:12:33 -0300

    Room and chat init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const createSocket = require("./socket");
+
+const clientURL = process.env.CLIENT_URL || "*";
 
 const app = express();
+app.use(cors({ origin: clientURL }));
+app.use(helmet());
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => `Server listening at http://localhost:${port}`);
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`)
+);
+
+const socket = createSocket(server, {
+  cors: { origin: clientURL },
+});
--- backend/src/socket.js
+++ backend/src/socket.js
@@ -1,19 +1,71 @@
 const { Server } = require("socket.io");
 const { v4: uuid } = require("uuid");
 
-const io = new Server(server);
+function createSocket(server, options = {}) {
+  const io = new Server(server, options);
 
-io.on("connection", (socket) => {
-  const id = uuid();
-  console.log(`Client ${id} connected!`);
+  const rooms = {
+    test: {
+      id: uuid(),
+      messages: [],
+      players: {},
+    },
+  };
 
-  io.on("message", (data) => {
-    console.log(`Client ${id} send:`, data);
+  io.on("connection", (socket) => {
+    const player = { id: socket.id, roomId: null };
+
+    console.log(`Client ${player.id} connected!`);
+
+    socket.on("join-room", (roomId) => {
+      console.log(`Client ${player.id} join room ${roomId}!`);
+      if (roomId in rooms) {
+        player.roomId = roomId;
+        rooms[roomId].players[player.id] = player;
+        socket.emit("join-room", true);
+      } else {
+        socket.emit("join-room", false);
+      }
+    });
+
+    socket.on("chat-message", (data) => {
+      console.log(`Client ${player.id} send:`, data, player);
+      if (player.roomId in rooms) {
+        rooms[player.roomId].messages.push({
+          id: uuid(),
+          from: player.id,
+          message: data,
+        });
+        // Send self
+        socket.emit("chat-message", rooms[player.roomId].messages);
+        // Send to others
+        Object.keys(rooms[player.roomId].players).map((id) => {
+          socket.to(id).emit("chat-message", rooms[player.roomId].messages);
+        });
+      }
+    });
+
+    socket.on("disconnecting", (reason) => {
+      if (player.roomId in rooms) {
+        Object.keys(rooms[player.roomId].players).map((id) => {
+          rooms[player.roomId].messages.push({
+            id: uuid(),
+            from: player.id,
+            message: player.id + " has left!",
+          });
+          socket.to(id).emit("chat-message", rooms[player.roomId].messages);
+          socket.to(id).emit("player-left", player.id);
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Client ${player.id} disconected!`);
+    });
   });
 
-  socket.on("close", () => console.log(`Client ${id} disconected!`));
-});
+  setInterval(() => io.emit("server-time", new Date().toISOString()), 1000);
+  return io;
+}
 
-setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
-
-module.exports = io;
+module.exports = createSocket;
