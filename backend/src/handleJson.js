044a4f6adc1aae24d8e6ae31433d12944d897678
bielzin <oliva.gabriel@outlook.com>
2021-02-04 11:12:46 -0300

    json formatter added

---
+++ backend/src/handleJson.js
@@ -0,0 +1,16 @@
+const short_json = (big_json) => {
+  new_json = {};
+  new_json["total_songs"] = big_json["total"];
+  new_json["songs"] = big_json["items"].map(item => {
+    return {
+      "name": item.track.name,
+      "artist": item.track.artists[0].name,
+      "spotify_url": item.track.external_urls.spotify,
+      "api_url": item.track.href,
+    }
+  }
+  )
+  return new_json;
+}
+
+export default short_json;
