98164dee384850ddbca58c143c8c9903e5d7d5b5
KaesarZ <kaesar.dev@gmail.com>
2021-04-21 16:12:46 -0300

    Update server add controlelrs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,20 @@
 import App from './app';
 import { AuthController } from './controllers/auth.controller';
-// import { UserController } from './controllers/user.controller';
+import { GenreController } from './controllers/genre.controller';
+import { MessageController } from './controllers/message.controller';
+import { MusicController } from './controllers/music.controller';
+import { PlayController } from './controllers/play.controller';
+import { RoomController } from './controllers/room.controller';
+import { UserController } from './controllers/user.controller';
 
 const app = new App([
   new AuthController(),
-  //  new UserController()
+  new UserController(),
+  new RoomController(),
+  new MessageController(),
+  new MusicController(),
+  new PlayController(),
+  new GenreController(),
 ]);
 
 app.listen();
