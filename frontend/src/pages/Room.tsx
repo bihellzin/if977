dea1d69aaa8af0f1c10e55183647602e14c9aa7f
KaesarZ <kaesar.dev@gmail.com>
2021-04-21 14:38:57 -0300

    Add auth

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -4,12 +4,6 @@ import { SocketContext } from '../services/socket';
 import Textarea from '../components/Textarea';
 import { Row, Col, Form, InputGroup } from 'react-bootstrap';
 import { GrClose, AiOutlineSend } from 'react-icons/all';
-import avatar1 from '../assets/avatar-1.png';
-import avatar2 from '../assets/avatar-2.png';
-import avatar3 from '../assets/avatar-3.png';
-import avatar4 from '../assets/avatar-4.png';
-import avatar5 from '../assets/avatar-5.png';
-import avatar6 from '../assets/avatar-6.png';
 
 function Room() {
   const history = useHistory();
