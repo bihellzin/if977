bdc955e7333ed1ddae0049cad59a27a5c6017c04
KaesarZ <kaesar.dev@gmail.com>
2021-04-21 22:57:15 -0300

    Update database settings and .gitignore

--- backend/src/databases/index.ts
+++ backend/src/databases/index.ts
@@ -17,9 +17,25 @@ class Database {
 
   static async createConnection() {
     if (Database.connection == null || !Database.connection.isConnected) {
+      const env = process.env.NODE_ENV;
       const defaultOptions = await getConnectionOptions();
-      Database.connection = await createConnection(defaultOptions);
-      Database.connection.runMigrations();
+      Database.connection = await createConnection(
+        Object.assign(
+          defaultOptions,
+          env === 'production'
+            ? ({
+                url: process.env.DATABASE_URL,
+                synchronize: true,
+                logging: false,
+                dropSchema: true,
+                entities: ['./dist/models/*.model.js'],
+                migrations: ['./dist/databases/migration/**/*.js'],
+                subscribers: ['./dist/databases/subscriber/**/*.js'],
+              } as ConnectionOptions)
+            : defaultOptions,
+        ),
+      );
+      await Database.connection.runMigrations();
     }
     return Database.connection;
   }
